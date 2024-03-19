6d223f76525cf50d1b47b3be96d95fbd9da3e586
Patrick-Hoff <139597982+Patrick-Hoff@users.noreply.github.com>
2024-03-18 21:37:18 -0300

    commitado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 
 import Header from "./components/header/Header"
+import Footer from "./components/footer/Footer"
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <h1>Hello World!</h1>
+      <p>Olá Mundo! Estou aprendendo React JS!</p>
+      <Footer />
     </div>
   );
 }
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,6 +1,10 @@
+import styles from "./Footer.module.css"
+
 function Footer() {
     return (
-        <footer></footer>
+        <footer className={styles.footer}>
+            <h2>MaiaFlix &copy; Desenvolvido por Edson Maia 2024</h2>
+        </footer>
     )
 }
 
